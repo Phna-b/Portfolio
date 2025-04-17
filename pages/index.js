b427aa9e7c9b265aac8c8cfac6d96cd6dc782d46
Pedro Henrique Nunes de Assis <phna8589@gmail.com>
2025-04-17 12:02:35 -0300

    [FEAT] Integração Chakra-ui

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
+import {Container, Box, Heading} from '@chakra-ui/react'
 const Page = () => {
     return (
-        <div> Hello </div>
+        <Container>
+            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+                Olá, sou desenvolvedor back-end e especialista em IBM MAXIMO do Brasil!
+            </Box>
+
+            <Box display={{md:'flex'}}>
+            <Box flexGrow={1}>
+                <Heading as="h2" variant="page-tittle">
+                Pedro Nunes
+                </Heading>
+
+                <p> Consultor IBM MAXIMO / Desenvolvedor / Graduando em Sistemas de Informação </p>
+            </Box>
+            </Box>
+        </Container>
+ 
     )
 }
 
-export default Page
+export default Page 
