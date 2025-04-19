0ccc1a296426f4e1f9f809f7732d466227e3d760
Pedro Henrique Nunes de Assis <phna8589@gmail.com>
2025-04-19 14:10:50 -0300

    [FEAT] Profile image

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,40 @@
-import {Container, Box, Heading} from '@chakra-ui/react'
+import {Container, Box, Heading, Image, useColorModeValue} from '@chakra-ui/react'
 const Page = () => {
     return (
         <Container>
-            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+            <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
                 Olá, sou desenvolvedor back-end e especialista em IBM MAXIMO do Brasil!
             </Box>
 
             <Box display={{md:'flex'}}>
-            <Box flexGrow={1}>
-                <Heading as="h2" variant="page-tittle">
-                Pedro Nunes
-                </Heading>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-tittle">
+                    Pedro Nunes
+                    </Heading>
+                    <p> Consultor IBM MAXIMO / Desenvolvedor / Graduando em Sistemas de Informação </p>
+                </Box>
 
-                <p> Consultor IBM MAXIMO / Desenvolvedor / Graduando em Sistemas de Informação </p>
-            </Box>
+                <Box
+                    flexShrink={0}
+                    mt={{ base: 4, md: 0 }}
+                    ml={{ md: 6 }}
+                    textAlign="center">
+                        <Box
+                            borderColor="whiteAlpha.800"
+                            borderWidth={2}
+                            borderStyle="solid"
+                            w="100px"
+                            h="100px"
+                            display="inline-block"
+                            borderRadius="full"
+                            overflow="hidden">
+                                <Image
+                                src="/images/Pedro.jpg"
+                                alt="Profile image"
+                                width="100"
+                                height="100"/>
+                        </Box>
+                 </Box>
             </Box>
         </Container>
  
