05b61f6c14e01851d5e1934348512ea13ae94db1
Pedro Henrique Nunes de Assis <phna8589@gmail.com>
2025-04-29 01:05:22 -0300

    [HOTFIX] Correção no path de trabalhos

--- components/work.js
+++ components/work.js
@@ -1,22 +1,25 @@
 import NextLink from 'next/link'
 import { Heading, Box, Image, Link, Badge } from '@chakra-ui/react'
 import { ChevronRightIcon } from '@chakra-ui/icons'
+import { useTranslation } from 'react-i18next';
 
-export const Title = ({ children }) => (
-  <Box>
-    <Link as={NextLink} href="/works">
-      Works
-    </Link>
-    <span>
-      {' '}
-      <ChevronRightIcon />{' '}
-    </span>
-    <Heading display="inline-block" as="h3" fontSize={20} mb={4}>
-      {children}
-    </Heading>
-  </Box>
-)
-
+export const Title = ({ children }) =>{
+  const { t } = useTranslation();
+  return(
+    <Box>
+      <Link as={NextLink} href="/works">
+      {t('trabalho')}
+      </Link>
+      <span>
+        {' '}
+        <ChevronRightIcon />{' '}
+      </span>
+      <Heading display="inline-block" as="h3" fontSize={20} mb={4}>
+        {children}
+      </Heading>
+    </Box>
+  )
+}
 export const WorkImage = ({ src, alt }) => (
   <Image borderRadius="lg" w="full" src={src} alt={alt} mb={4} />
 )
